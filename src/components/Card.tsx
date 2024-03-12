dbced6986e224810e939f07bfa581a25003c1722
Michael Wolf Hoffman <michaelhoffmanj@gmail.com>
2024-03-11 19:44:53 -0600

    layout progress

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,18 +1,36 @@
-import { Component } from "react";
+import { ReactElement } from "react";
 import styles from "./Card.module.css";
+import { useMediaQuery } from "@mui/material";
 
 type CardProps = {
   title: string;
   description: string;
-  child?: Component | null;
+  child?: ReactElement | null;
 };
 
 export default function Card({ title, description, child }: CardProps) {
-  return (
-    <div className={styles.cardContainer}>
-      <h1 className={styles.cardTitle}>{title}</h1>
-      <p className={styles.cardDescription}>{description}</p>
-      {child ? <>child</> : null}
-    </div>
-  );
+  const isMobile = useMediaQuery("(max-width: 600px)");
+
+  const renderMobileLayout = () => {
+    return (
+      <div className={styles.cardContainer}>
+        <h1 className={styles.cardTitle}>{title}</h1>
+        <p className={styles.cardDescription}>{description}</p>
+        {child ? <>child</> : null}
+      </div>
+    );
+  };
+
+  const renderDesktopLayout = () => {
+    return (
+      <div className={styles.cardContainer}>
+        <div className={styles.desktopSidebar}></div>
+        <h1 className={styles.cardTitle}>{title}</h1>
+        <p className={styles.cardDescription}>{description}</p>
+        {child ? <>child</> : null}
+      </div>
+    );
+  };
+
+  return <>{isMobile ? renderMobileLayout() : renderDesktopLayout()}</>;
 }
--- src/components/MultiStepForm.tsx
+++ src/components/MultiStepForm.tsx
@@ -1,4 +1,9 @@
+import { Component, ReactElement } from "react";
 import Card from "./Card";
+
+function FormContent(): ReactElementl {
+  return <>form content...</>;
+}
 
 export default function MultiStepForm() {
   return (
@@ -6,6 +11,7 @@ export default function MultiStepForm() {
       <Card
         title="Personal info"
         description="Please provide your name, email, address, and phone number."
+        child={FormContent}
       />
     </>
   );
