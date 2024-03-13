6fb4841335b75707abc87b4f016f75b01716b30f
Michael Wolf Hoffman <michaelhoffmanj@gmail.com>
2024-03-12 19:05:23 -0600

    starting multistep form for mobile layout

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,36 +1,17 @@
-import { ReactElement } from "react";
+import { ReactNode } from "react";
 import styles from "./Card.module.css";
 import { useMediaQuery } from "@mui/material";
 
 type CardProps = {
-  title: string;
-  description: string;
-  child?: ReactElement | null;
+  children: ReactNode;
 };
 
-export default function Card({ title, description, child }: CardProps) {
-  const isMobile = useMediaQuery("(max-width: 600px)");
+export default function Card({ children }: CardProps) {
+  // const isMobile = useMediaQuery("(max-width: 600px)");
 
-  const renderMobileLayout = () => {
-    return (
-      <div className={styles.cardContainer}>
-        <h1 className={styles.cardTitle}>{title}</h1>
-        <p className={styles.cardDescription}>{description}</p>
-        {child ? <>child</> : null}
-      </div>
-    );
-  };
-
-  const renderDesktopLayout = () => {
-    return (
-      <div className={styles.cardContainer}>
-        <div className={styles.desktopSidebar}></div>
-        <h1 className={styles.cardTitle}>{title}</h1>
-        <p className={styles.cardDescription}>{description}</p>
-        {child ? <>child</> : null}
-      </div>
-    );
-  };
-
-  return <>{isMobile ? renderMobileLayout() : renderDesktopLayout()}</>;
+  return (
+    <>
+      <div className={styles.cardContainer}>{children}</div>
+    </>
+  );
 }
--- src/components/MultiStepForm.tsx
+++ src/components/MultiStepForm.tsx
@@ -1,18 +1,38 @@
-import { Component, ReactElement } from "react";
+import { FormEvent } from "react";
 import Card from "./Card";
-
-function FormContent(): ReactElementl {
-  return <>form content...</>;
-}
+import styles from "./MultiStepForm.module.css";
+import Button from "./Button";
 
 export default function MultiStepForm() {
+  const handleSubmit = (e: FormEvent) => {
+    e.preventDefault();
+    console.log("submitting");
+  };
+
   return (
     <>
-      <Card
-        title="Personal info"
-        description="Please provide your name, email, address, and phone number."
-        child={FormContent}
-      />
+      <Card>
+        <h1 className={styles.formTitle}>Personal info</h1>
+        <p className={styles.formDescription}>
+          Please provide your name, email, address, and phone number.
+        </p>
+
+        <form className={styles.form} onSubmit={handleSubmit}>
+          <label htmlFor="step1-name">Name</label>
+          <input id="step1-name" type="text" name="name" />
+          <br />
+
+          <label htmlFor="step1-email">Email Address</label>
+          <input id="step1-email" type="email" name="email" />
+          <br />
+
+          <label htmlFor="step1-phone">Phone Number</label>
+          <input type="phone" id="step1-phone" name="phone" />
+          <br />
+
+          <button className={`button ${styles.bottomRight}`}>Next Step</button>
+        </form>
+      </Card>
     </>
   );
 }
