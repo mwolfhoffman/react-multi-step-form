9e4e1673ca374831c03d369dee87a2418e470e72
Michael Wolf Hoffman <michaelhoffmanj@gmail.com>
2024-03-15 15:25:01 -0600

    starting addons. (still need to handle required billing plan)

--- src/features/MultiStepForm/components/AddOnsStep.tsx
+++ src/features/MultiStepForm/components/AddOnsStep.tsx
@@ -1,12 +1,46 @@
+import styles from "./MultiStepForm.module.css";
+
+interface AddOnOption {
+  name: string;
+  description: string;
+  selected: boolean;
+  monthlyCost: number;
+  yearlyCost: number;
+}
+
 export default function AddOnsStep() {
+  const addOnOptions: AddOnOption[] = [
+    {
+      name: "Online service",
+      description: "Access to multiplayer games",
+      selected: false,
+      monthlyCost: 1,
+      yearlyCost: 10,
+    },
+    {
+      name: "Larget storage",
+      description: "Extra 1TB of cloud save",
+      selected: false,
+      monthlyCost: 2,
+      yearlyCost: 20,
+    },
+    {
+      name: "Customizable profile",
+      description: "Customize theme on your profile",
+      selected: false,
+      monthlyCost: 2,
+      yearlyCost: 20,
+    },
+  ];
+
   return (
     <>
       <h3>Pick Addons</h3>
-      <ul>
-        <li>Online service</li>
-        <li>Larget Storage</li>
-        <li>Customizable Profile</li>
-      </ul>
+      {addOnOptions.map((option) => (
+        <>
+          <h1>{option.name}</h1>
+        </>
+      ))}
     </>
   );
 }
