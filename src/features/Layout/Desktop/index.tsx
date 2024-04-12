2e4b4cc84afe5ffda1fc77ef88ae26720358d6e9
mwholfhoffman <michaelhoffmanj@gmail.com>
2024-04-12 13:17:54 -0600

    starting desktop layout

--- src/features/Layout/Desktop/index.tsx
+++ src/features/Layout/Desktop/index.tsx
@@ -2,10 +2,15 @@ import MultiStepForm from "../../MultiStepForm/components"
 
 function DesktopLayout(){
     return (  
-    <>
-    <aside>aside</aside>
+    <div className="flex">
+    
+    <aside className="w-1/3">
+    <img src='../../assets/images/bg-sidebar-desktop.svg' className="h-auto" alt="mobile-hero-image"/>
+      </aside>
+      <main className="w-2/3">
         <MultiStepForm />
-      </>)
+      </main>
+      </div>)
 }
 
 export default DesktopLayout
