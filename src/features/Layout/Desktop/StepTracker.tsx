076416b03b5a4c5b2b01f7a9b87aa18bbf111a29
mwholfhoffman <michaelhoffmanj@gmail.com>
2024-04-26 12:14:27 -0600

    desktop layout compelte

--- src/features/Layout/Desktop/StepTracker.tsx
+++ src/features/Layout/Desktop/StepTracker.tsx
@@ -1,23 +1,28 @@
-import { useFormStateContext } from '../../MultiStepForm/context/FormStateContext'
+import { useFormStateContext } from "../../MultiStepForm/context/FormStateContext";
 
-function StepTracker() {
-  const { steps, currentStepIndex } = useFormStateContext()
+function StepTracker(){
 
-  return (
-    <div className="flex flex-col space-y-10 text-alabaster bg-hero-desktop bg-center bg-cover h-screen w-64 m-4 rounded-lg">
-      <div className="p-10">
-        {steps.map((step, idx) => (
-          <div
-            key={idx}
-            className={`${currentStepIndex === idx ? 'bg-pastelBlue text-marineBlue' : ''} m-10 w-8 h-8 border border-white-500 rounded-full flex items-center justify-center`}
-          >
-            <div>{idx + 1}</div>
-            <div>Step {idx + 1}</div>
-          </div>
+    const { steps, currentStepIndex } = useFormStateContext();
+
+return(
+    <div className="flex flex-col space-y-10 text-alabaster bg-hero-desktop bg-center bg-cover h-screen w-96 m-4 rounded-lg">
+       <div className="p-10">
+            {steps.map((step, idx) => (
+                <div key={idx} className="flex items-baseline">
+                    <div className={`${currentStepIndex === idx ? 'bg-pastelBlue text-marineBlue' : ''} m-10 w-8 h-8 border border-white-500 rounded-full flex items-center justify-center`}>
+                        {idx + 1}
+                    </div>
+                    <div>
+                        Step {idx+1}
+                        <br/>
+                        <div className="font-bold">
+                        {step.description}
+                        </div>
+                    </div>
+            </div>
         ))}
       </div>
-    </div>
-  )
+    </div>)
 }
 
 export default StepTracker
