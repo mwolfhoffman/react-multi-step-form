99e7140a80d7a5d0680132cc43a621fc6f59e313
mwholfhoffman <michaelhoffmanj@gmail.com>
2024-04-26 13:57:10 -0600

    info page styled

--- src/features/Layout/Desktop/StepTracker.tsx
+++ src/features/Layout/Desktop/StepTracker.tsx
@@ -1,28 +1,28 @@
-import { useFormStateContext } from "../../MultiStepForm/context/FormStateContext";
+import { useFormStateContext } from '../../MultiStepForm/context/FormStateContext'
 
-function StepTracker(){
+function StepTracker() {
+  const { steps, currentStepIndex } = useFormStateContext()
 
-    const { steps, currentStepIndex } = useFormStateContext();
-
-return(
+  return (
     <div className="flex flex-col space-y-10 text-alabaster bg-hero-desktop bg-center bg-cover h-100vh w-96 m-4 rounded-lg">
-       <div className="p-10">
-            {steps.map((step, idx) => (
-                <div key={idx} className="flex items-baseline">
-                    <div className={`${currentStepIndex === idx ? 'bg-pastelBlue text-marineBlue' : ''} m-10 w-8 h-8 border border-white-500 rounded-full flex items-center justify-center`}>
-                        {idx + 1}
-                    </div>
-                    <div>
-                        Step {idx+1}
-                        <br/>
-                        <div className="font-bold">
-                        {step.description}
-                        </div>
-                    </div>
+      <div className="p-10">
+        {steps.map((step, idx) => (
+          <div key={idx} className="flex items-baseline">
+            <div
+              className={`${currentStepIndex === idx ? 'bg-pastelBlue text-marineBlue' : ''} m-10 w-8 h-8 border border-white-500 rounded-full flex items-center justify-center`}
+            >
+              {idx + 1}
             </div>
+            <div>
+              Step {idx + 1}
+              <br />
+              <div className="font-bold">{step.description}</div>
+            </div>
+          </div>
         ))}
       </div>
-    </div>)
+    </div>
+  )
 }
 
 export default StepTracker
--- src/features/MultiStepForm/components/PersonalInfoStep.tsx
+++ src/features/MultiStepForm/components/PersonalInfoStep.tsx
@@ -18,50 +18,72 @@ export default function PersonalInfoStep() {
 
   return (
     <>
-      <h1 className={styles.formTitle}>Personal info</h1>
-      <p className={styles.formDescription}>
+      <h1 className="text-4xl font-bold text-marineBlue mb-4">Personal info</h1>
+      <p className=" text-coolGray mb-8">
         Please provide your name, email, address, and phone number.
       </p>
-      <label htmlFor="step1-name">Name</label>
-      {formErrors.name && (
-        <span className={styles.errorLabel}>{formErrors.name}</span>
-      )}
-      <input
-        autoFocus
-        id="step1-name"
-        type="text"
-        name="name"
-        value={formState.name}
-        onChange={handleChange}
-        onFocus={handleFocus}
-      />
-      <br />
-      <label htmlFor="step1-email">Email Address</label>
-      {formErrors.email && (
-        <span className={styles.errorLabel}>{formErrors.email}</span>
-      )}{' '}
-      <input
-        id="step1-email"
-        type="text"
-        name="email"
-        value={formState.email}
-        onChange={handleChange}
-        onFocus={handleFocus}
-      />
-      <br />
-      <label htmlFor="step1-phone">Phone Number</label>
-      {formErrors.phone && (
-        <span className={styles.errorLabel}>{formErrors.phone}</span>
-      )}
-      <input
-        type="tel"
-        id="step1-phone"
-        name="phone"
-        value={formState.phone}
-        onChange={handleChange}
-        onFocus={handleFocus}
-      />
-      <br />
+
+      <form>
+        <div className="mb-4">
+          <label
+            htmlFor="step1-name"
+            className="text-marineBlue text-sm font-light"
+          >
+            Name
+          </label>
+          {formErrors.name && (
+            <span className={styles.errorLabel}>{formErrors.name}</span>
+          )}
+          <input
+            autoFocus
+            id="step1-name"
+            type="text"
+            name="name"
+            value={formState.name}
+            onChange={handleChange}
+            onFocus={handleFocus}
+          />
+        </div>
+
+        <div className="mb-4">
+          <label
+            htmlFor="step1-email"
+            className="text-marineBlue text-sm font-light"
+          >
+            Email Address
+          </label>
+          {formErrors.email && (
+            <span className={styles.errorLabel}>{formErrors.email}</span>
+          )}{' '}
+          <input
+            id="step1-email"
+            type="text"
+            name="email"
+            value={formState.email}
+            onChange={handleChange}
+            onFocus={handleFocus}
+          />
+        </div>
+        <div className="mb-4">
+          <label
+            htmlFor="step1-phone"
+            className="text-marineBlue text-sm font-light"
+          >
+            Phone Number
+          </label>
+          {formErrors.phone && (
+            <span className={styles.errorLabel}>{formErrors.phone}</span>
+          )}
+          <input
+            type="tel"
+            id="step1-phone"
+            name="phone"
+            value={formState.phone}
+            onChange={handleChange}
+            onFocus={handleFocus}
+          />
+        </div>
+      </form>
     </>
   )
 }
