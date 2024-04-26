e03cc4a6b0d601fc08ccd86067264c690560100e
mwholfhoffman <michaelhoffmanj@gmail.com>
2024-04-26 11:14:31 -0600

    mobile step tracker styled correctly

--- src/features/Layout/Mobile/index.tsx
+++ src/features/Layout/Mobile/index.tsx
@@ -1,27 +1,24 @@
-import MultiStepForm from "../../MultiStepForm/components"
-import { useFormStateContext } from "../../MultiStepForm/context/FormStateContext"
+import MultiStepForm from "../../MultiStepForm/components";
+import { useFormStateContext } from "../../MultiStepForm/context/FormStateContext";
 
-function MobileLayout(){
-    //  TODO: show current step 
-    const { steps, currentStepIndex } = useFormStateContext()
-    console.log(currentStepIndex, steps)
+function MobileLayout() {
+  //  TODO: show current step 
+  const { steps, currentStepIndex } = useFormStateContext();
 
-    return (
-    <div className="relative">
-        <img src='../../assets/images/bg-sidebar-mobile.svg' className="w-full h-auto" alt="mobile-hero-image"/>
-        <div className="flex justify-center absolute top-8 right-16 text-white">
-            {steps.map((step,idx)=>(
-                <div className={`${currentStepIndex === idx? 'bg-red-500': ''} mx-4
-                 w-8 h-8 border border-white-500 rounded-full text-center`}>
-                    {idx + 1}
-                </div>
-            ))}
-        </div>
-        <div className="absolute top-48 left-0 w-full z-10 bg-white -mt-6">
-            <MultiStepForm  />
-        </div>
+  return (
+    <div className="h-screen flex flex-col">
+      <div className="flex justify-center items-center text-alabaster bg-hero-mobile bg-center bg-cover h-40">
+        {steps.map((step, idx) => (
+          <div key={idx} className={`${currentStepIndex === idx ? 'bg-pastelBlue text-marineBlue' : ''} mx-4 w-8 h-8 border border-white-500 rounded-full  flex items-center justify-center`}>
+            {idx + 1}
+          </div>
+        ))}
       </div>
-      )
+      <div className="flex-grow bg-white px-6">
+          <MultiStepForm />
+      </div>
+    </div>
+  );
 }
 
-export default MobileLayout
+export default MobileLayout;
--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,30 @@ export default {
       backgroundImage: {
         'hero-mobile': "url('./assets/images/bg-sidebar-mobile.svg')",
       },
+      colors: {
+        marineBlue: 'hsl(213, 96%, 18%)',
+        purplishBlue: 'hsl(243, 100%, 62%)',
+        pastelBlue: 'hsl(228, 100%, 84%)',
+        lightBlue: 'hsl(206, 94%, 87%)',
+        strawberryRed: 'hsl(354, 84%, 57%)',
+        pastelBlue: 'hsl(228, 100%, 84%)',
+        coolGray: 'hsl(231, 11%, 63%)',
+        lightGray: 'hsl(229, 24%, 87%)',
+        magnolia: 'hsl(217, 100%, 97%)',
+        alabaster: 'hsl(231, 100%, 99%)',
+        white: 'hsl(0, 0%, 100%)'
+      },
+      fontFamily: {
+        body: ['Ubuntu', 'sans-serif'],
+      },
+      fontSize: {
+        'body': '16px',
+      },
+      fontWeight: {
+        'normal': 400,
+        'medium': 500,
+        'bold': 700,
+      },
     },
   },
   plugins: [],
