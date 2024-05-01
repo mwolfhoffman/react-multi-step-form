a3cf1eb4cbb848d712b2b041dc10c6ce68933a32
Wolf Hoffman <michaelhoffmanj@gmail.com>
2024-05-01 14:02:44 -0600

    Yup (#5)
    * form field validation
    
    * fix lint

---
+++ src/features/MultiStepForm/validations/personalInfo.ts
@@ -0,0 +1,14 @@
+import { object, string } from 'yup'
+
+const phoneRegExp =
+  /^((\\+[1-9]{1,4}[ \\-]*)|(\\([0-9]{2,3}\\)[ \\-]*)|([0-9]{2,4})[ \\-]*)*?[0-9]{3,4}?[ \\-]*[0-9]{3,4}?$/
+
+const personalInfoSchema = object().shape({
+  name: string().required('Name is required.'),
+  email: string().email('Email must be valid.').required('Email is required.'),
+  phone: string()
+    .matches(phoneRegExp, 'Phone number is not valid')
+    .required('Phone is required.'),
+})
+
+export default personalInfoSchema
